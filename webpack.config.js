2d9377b300d94a1e00e8b6c851c8519ac2da08a0
RitaLanca <rita.lanca17@gmail.com>
2025-10-09 15:03:43 +0100

    connect webpack-plugin-serve to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const { mode } = require("webpack-nano/argv");
 const {
   MiniHtmlWebpackPlugin,
 } = require("mini-html-webpack-plugin");
+const { WebpackPluginServe } = require('webpack-plugin-serve');
 
 module.exports = {
-  mode,
+  watch: mode === "development",
+  entry: [
+    "./src",
+    'webpack-plugin-serve/client' // ← important: this is required, where the magic happens in the browser
+  ],
+  mode, // the current mode => production, development, etc
   plugins: [
     new MiniHtmlWebpackPlugin({ context: { title: "Demo" } }),
+    new WebpackPluginServe({
+      port: parseInt(process.env.PORT,10) || 3000,
+      static: "./dist",
+      liveReload: true,
+      waitForBuild: true
+    })
   ],
 };
