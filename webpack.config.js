36af6f2e540c0f16c951fed0bb9dbb5ab379372b
RitaLanca <rita.lanca17@gmail.com>
2025-10-09 17:39:31 +0100

    fix(dev-server live-reload): specify host to prevent websocket URL connection error due to [::] failing
    By default, webpack-plugin-serve uses host 0.0.0.0, which generated a websocket URL in IPV6 ([::]). Browsers cannot connect to [::], so live reload fails. So it was needed to explicitly setting host address

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     new MiniHtmlWebpackPlugin({ context: { title: "Demo" } }),
     new WebpackPluginServe({
+      host: "localhost", // "0.0.0.0"
       port: parseInt(process.env.PORT,10) || 3000,
       static: "./dist",
       liveReload: true,
